eb0de029ce0922df0062d16f9399bfefe1c35ff1
Rinku Salunke <rinkusalunke9@gmail.com>
2026-01-18 18:26:36 +0530

    Update BankManagementSystemApplication.java

--- BankManagementSystem/src/main/java/com/example/demo/BankManagementSystemApplication.java
+++ BankManagementSystem/src/main/java/com/example/demo/BankManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankManagementSystemApplication {
-
+s
 	public static void main(String[] args) {
 		SpringApplication.run(BankManagementSystemApplication.class, args);
 	}
